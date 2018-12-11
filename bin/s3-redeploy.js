3a58689bcf64d2258d3b1129c9f320e4e2ffebf5
Denis Fatkhudinov <denis.fatkhudinov@gmail.com>
2018-12-11 19:13:56 +0300

    Script updated

--- bin/s3-redeploy.js
+++ bin/s3-redeploy.js
@@ -4,7 +4,7 @@
 require('../src')(require('../src/lib/args-processor').parse(process.argv.slice(2)))
   .then(() => console.log('Execution complete'))
   .catch(err => {
-    console.log('Execution failed:');
-    console.log(err.message);
+    console.log('Execution failed:', err.message);
+    console.log('Original error:', err.originalError.message);
     process.exit(1);
   });
